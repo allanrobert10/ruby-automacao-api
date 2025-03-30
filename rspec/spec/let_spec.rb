46a84e78685c9ea00dab6aa0bf6fbd1d5a8ef861
allanrobert10 <allanrobert30@gmail.com>
2025-03-30 16:04:02 -0300

    aula 10 - trabalhando com let

---
+++ rspec/spec/let_spec.rb
@@ -0,0 +1,15 @@
+describe 'somar' do
+  context 'dois números' do
+    let(:resultado) {4+4}
+    let(:resultadoum) {resultado+4}
+    let(:resultadodois) {4+4}
+    let(:resultadotres) {4+4}
+    
+    it 'does something' do
+      expect(resultado). to eq 8
+      expect(resultadoum). to eq 12
+      expect(resultadodois). to eq 8
+      expect(resultadotres). to eq 8
+    end  
+  end
+end
